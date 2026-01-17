6b8beed46ee8c9a07d8cd9c1fb8d4e139a4504ba
Roykee <roykee@example.com>
2026-01-17 17:41:23 +0200

    Clean repository: Keep only essential files

---
+++ karny-bank/karny_bank/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Roykee\\OneDrive\\מסמכים\\karny-bank\\karny_bank" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\Roykee\\OneDrive\\מסמכים\\karny-bank\\karny_bank"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Roykee\\OneDrive\\מסמכים\\karny-bank\\karny_bank\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Roykee\\OneDrive\\מסמכים\\karny-bank\\karny_bank"
+  "FLUTTER_TARGET=C:\\Users\\Roykee\\OneDrive\\מסמכים\\karny-bank\\karny_bank\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzguNQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZjZmZjE1MjlmZA==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MTUyN2FlMGVjNQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC40"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Roykee\\OneDrive\\מסמכים\\karny-bank\\karny_bank\\.dart_tool\\package_config.json"
+)
